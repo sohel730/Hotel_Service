c2a889a776df45d115be3273bd8133cc6f7e1379
sohel730 <momin8805887375@gmail.com>
2026-04-21 19:25:21 +0530

    added @enabeldiscoveryclient annotation.

--- src/main/java/com/sohel/hotel/HotelAppApplication.java
+++ src/main/java/com/sohel/hotel/HotelAppApplication.java
@@ -2,9 +2,10 @@ package com.sohel.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class HotelAppApplication {
 
     public static void main(String[] args) {
